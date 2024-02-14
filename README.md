2101b18b65dd9eb109173dc1c89cd752d1072828
Tim Wuthenow <=>
2024-02-14 16:36:18 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # bamoe-canvas-quick-deploy
-Repository for quickly deploy BAMOE Canvas to OpenShift
+Repository for quickly deploying BAMOE Canvas to OpenShift.
+
+Login to your OpenShift cluster and pick the namespace that you want to deploy BAMOE Canvas to by running this script. The images are located at Quay.io, so you will need access to that repository to be able to use this. To register with Quay.io, you will need a Red Hat account. This account can be utilized with the free [Red Hat Developer Program](https://developers.redhat.com/articles/faqs-no-cost-red-hat-enterprise-linux) so you can get started quickly!
+
+On a Mac or Linux run `./canvas.sh` and the script will deploy your Canvas instance, the Extended Services and CORS-proxy for Git communication.
---
+++ canvas.sh
@@ -0,0 +1,53 @@
+    export APP_PART_OF=bamoe-canvas-app
+    export APP_NAME_EXTENDED_SERVICES=bamoe-extended-services
+    export APP_NAME_GIT_CORS_PROXY=bamoe-git-cors-proxy
+    export APP_NAME_BAMOE_CANVAS=bamoe-canvas
+    export VERSION=9.0.1
+
+       echo "Deploying Extended Services"
+ 
+    oc new-app quay.io/bamoe/extended-services:$VERSION --name=$APP_NAME_EXTENDED_SERVICES
+ 
+    oc create route edge --service=$APP_NAME_EXTENDED_SERVICES
+ 
+    oc label services/$APP_NAME_EXTENDED_SERVICES app.kubernetes.io/part-of=$APP_PART_OF
+ 
+    oc label routes/$APP_NAME_EXTENDED_SERVICES app.kubernetes.io/part-of=$APP_PART_OF
+ 
+    oc label deployments/$APP_NAME_EXTENDED_SERVICES app.kubernetes.io/part-of=$APP_PART_OF
+ 
+    oc label deployments/$APP_NAME_EXTENDED_SERVICES app.openshift.io/runtime=golang
+ 
+    echo "Deploying CORS"
+ 
+    oc new-app quay.io/bamoe/git-cors-proxy:$VERSION --name=$APP_NAME_GIT_CORS_PROXY
+ 
+    oc create route edge --service=$APP_NAME_GIT_CORS_PROXY
+ 
+    oc label services/$APP_NAME_GIT_CORS_PROXY app.kubernetes.io/part-of=$APP_PART_OF
+ 
+    oc label routes/$APP_NAME_GIT_CORS_PROXY app.kubernetes.io/part-of=$APP_PART_OF
+ 
+    oc label deployments/$APP_NAME_GIT_CORS_PROXY app.kubernetes.io/part-of=$APP_PART_OF
+ 
+    oc label deployments/$APP_NAME_GIT_CORS_PROXY app.openshift.io/runtime=nodejs
+ 
+    echo "Deploying Canvas"
+ 
+    oc new-app quay.io/bamoe/canvas:$VERSION --name=$APP_NAME_BAMOE_CANVAS \
+    -e KIE_SANDBOX_EXTENDED_SERVICES_URL=https://$(oc get route $APP_NAME_EXTENDED_SERVICES --output jsonpath={.spec.host}) \
+    -e KIE_SANDBOX_GIT_CORS_PROXY_URL=https://$(oc get route $APP_NAME_GIT_CORS_PROXY --output jsonpath={.spec.host}) \
+    -e 
+ 
+    oc create route edge --service=$APP_NAME_BAMOE_CANVAS
+ 
+    oc label services/$APP_NAME_BAMOE_CANVAS app.kubernetes.io/part-of=$APP_PART_OF
+ 
+    oc label routes/$APP_NAME_BAMOE_CANVAS app.kubernetes.io/part-of=$APP_PART_OF
+ 
+    oc label deployments/$APP_NAME_BAMOE_CANVAS app.kubernetes.io/part-of=$APP_PART_OF
+ 
+    oc label deployments/$APP_NAME_BAMOE_CANVAS app.openshift.io/runtime=js
+ 
+    echo "Canvas Route"
+    oc get route $APP_NAME_BAMOE_CANVAS --output jsonpath={.spec.host}
